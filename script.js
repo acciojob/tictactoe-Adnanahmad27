eb9e76f1a2de98f93e92a7a1cfb39af465ca1a68
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-08 19:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+function toggleForm(){
+	form.classLList.toggle("hide");
+	form.classLList.toggle("show");
+}
+function getForm(e){
+	e.preventDefault();
+	toggleForm();
+}
+form.addEventListener("submit" , getForm );
